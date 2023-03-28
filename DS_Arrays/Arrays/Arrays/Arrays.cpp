17d9aab1fc47b518197be333e247b4c73d24ee54
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2023-03-27 23:44:28 -0500

    Laptop Update
    ->Added many new features to Array. Union, Difference and Intersection to name a few.

--- DS_Arrays/Arrays/Arrays/Arrays.cpp
+++ DS_Arrays/Arrays/Arrays/Arrays.cpp
@@ -15,7 +15,6 @@ void swap(int* x, int* y)
 	temp = *x;
 	*x = *y;
 	*y = temp;
-
 }
 
 //ADT
@@ -33,6 +32,7 @@ void Display(struct Array arr)
 	printf("Elements are: \n");
 	for (i = 0; i < arr.length; i++)
 		printf("%d ", arr.A[i]);
+	std::cout << std::endl;
 }
 
 //Add an element to the end of the array
@@ -93,9 +93,295 @@ int LinearSearch(struct Array *arr, int key)
 			return 0;
 		}
 	}
-	return -1;//Invalid index
+	return -1;//Invalid index being returned so the function doesn't give you an index in the array. This is to say it failed.
 }
 
+int BinarySearch(struct Array arr, int key)
+{
+	int l, mid, h;
+	l = 0;
+	h = arr.length - 1;
+
+	while (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == arr.A[mid])
+			return mid;
+		else if (key < arr.A[mid])
+			h = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return -1;
+}
+
+int RecursiveBinarySearch(int a[], int l, int h, int key)
+{
+	int mid;
+
+	if (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == a[mid])
+			return mid;
+		else if (key < a[mid])
+			return RecursiveBinarySearch(a, l, mid - 1, key);
+		else
+			return RecursiveBinarySearch(a, mid + 1, h, key);
+	}
+
+	return -1;
+}
+
+int Get(struct Array arr, int index)
+{
+	if (index >= 0 && index < arr.length)
+		return arr.A[index];
+	return -1;
+}
+
+void Set(struct Array* arr, int index, int x)
+{
+	if (index >= 0 && index < arr->length)
+	{
+		arr->A[index] = x;
+	}
+}
+
+int Max(struct Array arr)
+{
+	int max = arr.A[0];
+	int i;
+	for (i = 1; i < arr.length; i++)
+		if (arr.A[i] > max)
+			max = arr.A[i];
+	return max;
+}
+
+
+int Min(struct Array arr)
+{
+	int min = arr.A[0];
+	int i;
+	for (i = 1; i < arr.length; i++)
+		if (arr.A[i] < min)
+			min = arr.A[i];
+	return min;
+}
+
+int Sum(struct Array arr)
+{
+	int sum = 0;
+	int i;
+	for (i = 0; i < arr.length; i++)
+		sum += arr.A[i];
+	return sum;
+}
+
+float Avg(struct Array arr)
+{
+	return (float)Sum(arr) / arr.length;
+}
+
+void Reverse(struct Array* arr)
+{
+	int* B;
+	int i, j;
+	//Array B should be size of array
+	B = (int*)malloc(arr->length * sizeof(int));
+	//Copy right of A to left of B
+	for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+		B[j] = arr->A[i];
+	for (i = 0; i < arr->length; i++)
+		arr->A[i] = B[i];
+}
+
+void Reverse2(struct Array* arr)
+{
+	int i, j;
+	for (i = 0, j = arr->length - 1; i < j; i++, j--)
+	{
+		swap(&arr->A[i], &arr->A[j]);
+	}
+}
+
+void LeftShift(struct Array* arr)
+{
+	int i;
+	for (i = 0; i < arr->length; i++)
+		arr->A[i] = arr->A[i + 1];
+}
+
+void RightShift(struct Array* arr)
+{
+	int i;
+	for (i = arr->length - 1; i > 0; i--)
+		arr->A[i] = arr->A[i - 1];
+}
+
+void LeftRotate(struct Array* arr)
+{
+	int i;
+	int temp = arr->A[0];
+	for (i = 0; i < arr->length; i++)
+		arr->A[i] = arr->A[i + 1];
+	arr->A[arr->length - 1] = temp;
+}
+
+void RightRotate(struct Array* arr)
+{
+	int i;
+	int temp = arr->A[arr->length - 1];
+	for (i = arr->length - 1; i > 0; i--)
+		arr->A[i] = arr->A[i - 1];
+	arr->A[0] = temp;
+}
+
+void InsertSort(struct Array* arr, int x)
+{
+	int i = arr->length - 1;
+	if (arr->length == arr->size)
+		return;
+	while (i >= 0 && arr->A[i] > x)
+	{
+		arr->A[i + 1] = arr->A[i];
+		i--;
+	}
+	arr->A[i + 1] = x;
+	arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+	int i;
+	for (i = 0; i < arr.length - 1; i++)
+	{
+		if (arr.A[i] > arr.A[i + 1])
+			return 0;
+	}
+	return 1;//If whole loop never returns 0 or false, return 1 or true
+}
+
+void REarrange(struct Array* arr)
+{
+	int i, j;
+	i = 0;
+	j = arr->length - 1;
+
+	while (i < j)
+	{
+		while (arr->A[i] < 0) i++;
+		while (arr->A[j] >= 0) j--;
+		if (i < j) swap(&arr->A[i], &arr->A[j]);
+	}
+
+}
+
+struct Array * Merge(struct Array* arr1, struct Array* arr2)
+{
+	int i, j, k;
+	i = j = k = 0;
+	struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));//Created on Heap
+
+	while (i < arr1->length && j < arr2->length)
+	{
+		if (arr1->A[i] < arr2->A[j])
+			arr3->A[k++] = arr1->A[i++];
+		else
+			arr3->A[k++] = arr2->A[j++];
+	}
+	//After one array finishes
+	for (; i < arr1->length; i++)
+		arr3->A[k++] = arr1->A[i];
+	for (; j < arr2->length; j++)
+		arr3->A[k++] = arr2->A[j];
+	arr3->length = arr1->length + arr2->length;
+	arr3->size = 10;
+
+	return arr3;
+}
+
+struct Array* Union(struct Array* arr1, struct Array* arr2)
+{
+	int i, j, k;
+	i = j = k = 0;
+	struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));//Created on Heap
+
+	while (i < arr1->length && j < arr2->length)
+	{
+		if (arr1->A[i] < arr2->A[j])
+			arr3->A[k++] = arr1->A[i++];
+		else if (arr2->A[j] < arr1->A[i])
+			arr3->A[k++] = arr2->A[j++];
+		else
+		{
+			arr3->A[k++] = arr1->A[i++];
+			j++;
+		}
+	}
+	//After one array finishes
+	for (; i < arr1->length; i++)
+		arr3->A[k++] = arr1->A[i];
+	for (; j < arr2->length; j++)
+		arr3->A[k++] = arr2->A[j];
+	arr3->length = k;
+	arr3->size = 10;
+
+	return arr3;
+}
+
+struct Array* Intersection(struct Array* arr1, struct Array* arr2)
+{
+	int i, j, k;
+	i = j = k = 0;
+	struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+	//Copy common elements
+	while (i < arr1->length && j < arr2->length)
+	{
+		if (arr1->A[i] < arr2->A[j])
+			i++;
+		else if (arr2->A[j] < arr1->A[i])
+			j++;
+		else
+		{
+			arr3->A[k++] = arr1->A[i++];
+			j++;
+		}
+	}
+
+	arr3->length = k;
+	arr3->size = 10;
+
+	return arr3;
+}
+
+struct Array* Difference(struct Array* arr1, struct Array* arr2)
+{
+	int i, j, k;
+	i = j = k = 0;
+	struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));//Created on Heap
+
+	while (i < arr1->length && j < arr2->length)
+	{
+		if (arr1->A[i] < arr2->A[j])
+			arr3->A[k++] = arr1->A[i++];
+		else if (arr2->A[j] < arr1->A[i])
+			j++;
+		else
+		{
+			i++;
+			j++;
+		}
+	}
+	for (; i < arr1->length; i++)
+		arr3->A[k++] = arr1->A[i];
+
+	arr3->length = k;
+	arr3->size = 10;
+
+	return arr3;
+}
 
 int main()
 {	//Functions
@@ -105,7 +391,20 @@ int main()
 	
 	//Array ADT
 	struct Array arr = { {2,4,6,8,10},10,5 };
-	
+	struct Array arr2 = { {-3, -5, -7, 9, 11, -13, -15, 17, 19}, 18, 9 };
+
+	//Merging Arrays
+	struct Array arr3 = { {2, 6, 15, 19, 25}, 10, 5 };
+	struct Array arr4 = { {3, 6, 7, 15, 29}, 10, 5 };
+	struct Array* arr5;
+
+	//Unsorted Arrays
+	struct Array arr6 = { {5, 3, 7, 1, 9}, 10, 5 };
+	struct Array arr7 = { {2, 3, 4, 10, 1}, 10, 5 };
+
+
+
+
 	/*Dynamically Create the Array. Use this with ADT
 	int n, i;
 	printf("Enter Size of an array: ");
@@ -125,8 +424,36 @@ int main()
 	//Append(&arr, 12);
 	//Insert(&arr, 5, 12);
 	//printf("%d\n", Delete(&arr, 4));
-	printf("%d\n", LinearSearch(&arr, 10));
-	Display(arr);
+	//printf("%d\n", LinearSearch(&arr, 10));
+	//printf("%d\n", BinarySearch(arr, 6));
+	//printf("%d\n", RecursiveBinarySearch(arr.A, 0, arr.length, 5));
+
+	//printf("%d\n", Get(arr, 2));
+	//Set(&arr, 0, 15);
+	//printf("%d\n", Max(arr));
+	//printf("%d\n", Min(arr));
+	//printf("%d\n", Sum(arr));
+	//printf("%f\n", Avg(arr));
+	
+	//Reverse(&arr);
+	//Reverse2(&arr);
+	//LeftShift(&arr);
+	//RightShift(&arr);
+	//LeftRotate(&arr);
+	//RightRotate(&arr);
+
+	//InsertSort(&arr, 1);
+	//printf("%d\n", isSorted(arr));
+	//REarrange(&arr2);
+	
+	//arr5 = Merge(&arr3, &arr4);
+
+	//arr5 = Union(&arr3, &arr4);
+	//arr5 = Intersection(&arr3, &arr4);
+	//arr5 = Difference(&arr3, &arr4);
+
+
+	Display(*arr5);
 }
 
 void ResizeArrays()
